054b5325c3815f4785067aa6a1a4cacd611722bf
dmt-denisov <double.dmt>
2023-10-13 15:34:40 +0300

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test actions
+          Test eslint
         </a>
       </header>
     </div>
